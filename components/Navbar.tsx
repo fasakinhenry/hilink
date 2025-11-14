b5ce27bd9d680145a04c83d6c366187aa39de0ce
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-14 16:49:18 +0100

    feat: create a new navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,25 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { NAV_LINKS } from '@/constants';
 
 const Navbar = () => {
   return (
-    <nav className='flexBetween z-30 relative py-5 max-container padding-container border-2 border-red-500'>
+    <nav className='flexBetween z-30 relative max-container padding-container flex border-2 border-red-500'>
       <Link href='/'>
-      <Image src="file.svg" alt="Logo" />
+        <Image src='/hilink-logo.svg' alt='Logo' width={74} height={29} />
       </Link>
+
+      <ul className='hidden h-full gap-12 lg:flex'>
+        {NAV_LINKS.map((link) => (
+          <Link href={link.href} key={link.key} className='regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'>
+            {link.label}
+          </Link>
+        ))}
+      </ul>
+
+      <div className="lg:flexCenter hidden">
+        <button>Click me</button>
+      </div>
     </nav>
   );
 };

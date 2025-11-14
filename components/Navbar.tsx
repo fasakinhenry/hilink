7d626231cdec9a3ed354085cc8b1189c604a587c
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-14 16:06:56 +0100

    chore: add all required images in public folder

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+import Image from 'next/image';
+
 const Navbar = () => {
   return (
     <nav className='flexBetween z-30 relative py-5 max-container padding-container border-2 border-red-500'>
-      Navbar
+      <Link href='/'>
+      <Image src="file.svg" alt="Logo" />
+      </Link>
     </nav>
   );
 };

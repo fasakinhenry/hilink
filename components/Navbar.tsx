a823f3b857854dd4f352c4d5d74909519f7f16f8
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-14 18:24:56 +0100

    style: fix tailwind styling and use v4

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,34 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { NAV_LINKS } from '@/constants';
+import Button from './Button';
 
 const Navbar = () => {
   return (
-    <nav className='flexBetween z-30 relative max-container padding-container flex border-2 border-red-500'>
+    <nav className='flexBetween z-30 relative max-container padding-container py-5'>
       <Link href='/'>
         <Image src='/hilink-logo.svg' alt='Logo' width={74} height={29} />
       </Link>
 
       <ul className='hidden h-full gap-12 lg:flex'>
         {NAV_LINKS.map((link) => (
-          <Link href={link.href} key={link.key} className='regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'>
+          <Link
+            href={link.href}
+            key={link.key}
+            className='regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'
+          >
             {link.label}
           </Link>
         ))}
       </ul>
 
-      <div className="lg:flexCenter hidden">
-        <button>Click me</button>
+      <div className='lg:flexCenter'>
+        <Button
+        type="button"
+        title="Login"
+        icon="/user.svg"
+        variant="btn_dark_green"
+        />
       </div>
     </nav>
   );

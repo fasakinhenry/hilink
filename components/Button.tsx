7606b03c400f65f5c27e447cf72f6e7346b4f8bb
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-15 19:20:46 +0100

    feat: implement the get app section

--- components/Button.tsx
+++ components/Button.tsx
@@ -4,14 +4,14 @@ type ButtonProps = {
   type: 'button' | 'submit';
   title: string;
   icon?: string;
-  variant: 'btn_dark_green' | 'btn_white_text' | 'btn_green' | 'btn_white';
+  variant: 'btn_dark_green' | 'btn_white_text' | 'btn_green' | 'btn_white' | 'btn_dark_green_outline';
   full?: boolean;
 };
 
-const Button = ({ type, title, icon, variant }: ButtonProps) => {
-  return <button type={type} className={`flexCenter gap-3 rounded-full border ${variant}`}>
+const Button = ({ type, title, icon, variant, full }: ButtonProps) => {
+  return <button type={type} className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}>
             {icon && <Image src={icon} alt={title} width={24} height={24} className="inline mr-2" />}
-            <label className="bold-16 ">{title}</label>
+            <label className="bold-16 cursor-pointer">{title}</label>
         </button>;
 };
 
--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,5 +1,36 @@
+import Image from "next/image";
+import Button from "./Button";
+
 const GetApp = () => {
-  return <section>GetApp</section>;
+  return (
+    <section className='flex items-center justify-center w-full flex-col pb-[100px]'>
+      <div className='mx-auto max-w-[1440px] relative flex w-full flex-col justify-between gap-32 overflow-hidden bg-green-90 bg-pattern bg-cover bg-center bg-no-repeat px-6 py-12 text-white sm:flex-row sm:gap-12 sm:py-24 lg:px-20 xl:max-h-[598px] 2xl:rounded-5xl'>
+        <div className='z-20 flex w-full flex-1 flex-col items-start justify-center gap-12'>
+          <h2 className="text-[40px] font-bold leading-[120%] lg:text-[64px] xl:max-w-[320px]">Get for free now!</h2>
+          <p className="text-[16px] font-normal text-gray-10">Available on IOS and Android</p>
+          <div className="flex w-full flex-col gap-3 whitespace-nowrap xl:flex-row">
+            <Button
+            type="button"
+            title="App store"
+            icon="/apple.svg"
+            variant="btn_white"
+            full
+            />
+            <Button
+            type="button"
+            title="Play Store"
+            icon="/android.svg"
+            variant="btn_dark_green_outline"
+            full
+            />
+          </div>
+        </div>
+        <div className="flex flex-1 items-center justify-end">
+          <Image src="/phones.png" alt="phone" width={550} height={870} />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default GetApp;

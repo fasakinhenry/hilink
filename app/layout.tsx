d7696167b8ce49cb3e0ea1cab11ff1ba054cf8b9
Fasakin Henry <fasakinhenry@gmail.com>
2025-11-15 05:02:41 +0100

    feat: add complete Hero section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,11 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
+import type { Metadata } from 'next';
+import './globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
 
 export const metadata: Metadata = {
-  title: "Travel",
-  description: "Travel UI/UX App for Camping",
+  title: 'Travel',
+  description: 'Travel UI/UX App for Camping',
 };
 
 export default function RootLayout({
@@ -14,12 +14,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
       <body>
         <Navbar />
-        <main>
-          {children}
-        </main>
+        <main className='relative overflow-hidden'>{children}</main>
         <Footer />
       </body>
     </html>
